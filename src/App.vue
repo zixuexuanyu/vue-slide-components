4d0da9d341143b3ad770bbcb0cabfba5f6b5a8da
zixuexuanyu <2983068126@qq.com>
2017-08-14 19:14:27 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <slideshow :slides='slides' :inv="slideSpeed"></slideshow>
+    <!-- <router-view></router-view> -->
+  </div>
+</template>
+
+<script>
+import slideshow from './components/layout'
+export default {
+  components:{
+    slideshow
+  },
+  data () {
+    return {
+      slideSpeed: 2000,
+      slides: [
+        {
+          src: require('./assets/images/1.jpg'),
+          title: 'xxx1',
+          href: 'detail/analysis'
+        },
+        {
+          src: require('./assets/images/2.jpg'),
+          title: 'xxx2',
+          href: 'detail/count'
+        },
+        {
+          src: require('./assets/images/3.jpg'),
+          title: 'xxx3',
+          href: 'http://xxx.xxx.com'
+        },
+        {
+          src: require('./assets/images/4.jpg'),
+          title: 'xxx4',
+          href: 'detail/forecast'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/layout.vue
@@ -0,0 +1,194 @@
+<template>
+  <div class="slide-show" @mouseover="clearInv" @mouseout="runInv">
+    <div class="slide-img">
+      <a :href="slides[nowIndex].href">
+        <transition name="slide-trans">
+          <img v-if="isShow" :src="slides[nowIndex].src">
+        </transition>
+        <transition name="slide-trans-old">
+          <img v-if="!isShow" :src="slides[nowIndex].src">
+        </transition>
+      </a>
+    </div>
+    <h2 class="slide-title">{{ slides[nowIndex].title }}</h2>
+    <ul class="slide-pages">
+      <li @click="goto(prevIndex)">&lt;</li>
+      <li v-for="(item, index) in slides" @click="goto(index)">
+        <a :class="{on: index === nowIndex}">{{ index + 1 }}</a>
+      </li>
+      <li @click="goto(nextIndex)">&gt;</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    slides: {
+      type: Array,
+      default: []
+    },
+    inv: {
+      type: Number,
+      default: 3000
+    }
+  },
+  data () {
+    return {
+      nowIndex: 0,
+      isShow: true
+    }
+  },
+  computed: {
+    prevIndex () {
+      if (this.nowIndex === 0) {
+        return this.slides.length - 1
+      }else {
+        return this.nowIndex - 1
+      }
+    },
+    nextIndex () {
+      if (this.nowIndex === this.slides.length - 1) {
+        return 0
+      }
+      else {
+        return this.nowIndex + 1
+      }
+    }
+  },
+  methods: {
+    goto (index) {
+      this.isShow = false
+      setTimeout(() => {
+        this.isShow = true
+        this.nowIndex = index
+      }, 10)
+    },
+    runInv () {
+      this.invId = setInterval(() => {
+        this.goto(this.nextIndex)
+      }, this.inv)
+    },
+    clearInv () {
+      clearInterval(this.invId)
+    }
+  },
+  mounted () {
+    this.runInv ()
+  }
+  
+}
+</script>
+
+<style scoped>
+html, body, div, span, applet, object, iframe,
+h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+a, abbr, acronym, address, big, cite, code,
+del, dfn, em, img, ins, kbd, q, s, samp,
+small, strike, strong, sub, sup, tt, var,
+b, u, i, center,
+dl, dt, dd, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td,
+article, aside, canvas, details, embed, 
+figure, figcaption, footer, header, hgroup, 
+menu, nav, output, ruby, section, summary,
+time, mark, audio, video {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  font-size: 100%;
+  font: inherit;
+  vertical-align: baseline;
+}
+/* HTML5 display-role reset for older browsers */
+article, aside, details, figcaption, figure, 
+footer, header, hgroup, menu, nav, section {
+  display: block;
+}
+body {
+  line-height: 1;
+}
+ol, ul {
+  list-style: none;
+}
+blockquote, q {
+  quotes: none;
+}
+blockquote:before, blockquote:after,
+q:before, q:after {
+  content: '';
+  content: none;
+}
+table {
+  border-collapse: collapse;
+  border-spacing: 0;
+}
+a {
+  color: inherit;
+  text-decoration: none;
+}
+body {
+  background: #f0f2f5;
+  font-family: "Helvetica Neue",Helvetica,Arial,"Hiragino Sans GB","Hiragino Sans GB W3","Microsoft YaHei UI","Microsoft YaHei","WenQuanYi Micro Hei",sans-serif;
+  font-size: 14px;
+  color: #444;
+}
+.slide-trans-enter-active {
+  transition: all .4s;
+}
+.slide-trans-enter {
+  transform: translateX(900px);
+}
+.slide-trans-old-leave-active {
+  transition: all .4s;
+  transform: translateX(-900px);
+}
+.slide-show {
+  position: relative;
+  margin: 15px 15px 15px 0;
+  width: 500px;
+  height: 300px;
+  overflow: hidden;
+}
+.slide-show h2 {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  color: #fff;
+  background: #000;
+  opacity: .5;
+  bottom: 0;
+  height: 30px;
+  text-align: left;
+  padding-left: 15px;
+}
+.slide-img {
+  width: 100%;
+}
+.slide-img img {
+  width: 100%;
+  position: absolute;
+  top: 0;
+}
+.slide-title{
+  line-height: 30px;
+}
+.slide-pages {
+  position: absolute;
+  bottom: 10px;
+  right: 15px;
+}
+.slide-pages li {
+  display: inline-block;
+  padding: 0 10px;
+  cursor: pointer;
+  color: #fff;
+}
+.slide-pages li a{
+  margin: 0 5px;
+}
+.slide-pages li .on {
+  text-decoration: underline;
+}
+</style>
